5b8512cbc4f17b5d488cb4aae03471a4e8ab9acb
Irina Vinitskaia <kreneva.i@yandex.ru>
2025-02-05 12:42:51 +0300

    fix: change isDriver-2

--- 6-Driver/index.js
+++ 6-Driver/index.js
@@ -1,7 +1,7 @@
 const isDriver = (hasLicence, age, isDrunk) => {
     const canDrive = hasLicence && age >= 18 && !isDrunk
-    // console.log(canDrive ? "Может" : "Не может");
-    return canDrive ? "Может" : "Не может"
+    console.log(canDrive ? "Может" : "Не может");
+    return canDrive;
 }
 
 isDriver(true, 18, false);
