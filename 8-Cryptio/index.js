4daf3a494cab16d20cd0e2fccf9f4f8ad1a3ef0e
Irina Vinitskaia <kreneva.i@yandex.ru>
2025-02-15 18:24:42 +0300

    feat: add function to decrypted password

---
+++ 8-Cryptio/index.js
@@ -0,0 +1,25 @@
+const crypto = (password) => {
+  let res = "";
+  let passString = password.toString();
+  if(passString.length % 2 === 0) {
+    for (let i = 0; i < passString.length; i+=2) {
+      res += passString[i+1] + passString[i];
+    } 
+  } else {
+    for (let i = 0; i < passString.length - 1; i+=2) {
+      res += passString[i+1] + passString[i];
+      }
+      res += passString[passString.length-1];
+    }
+    return res;
+};
+
+crypto(34567);
+
+const checkPassword = (password, userPassword) => {
+  const decryptedPass = crypto(password).toString();
+  return decryptedPass === userPassword.toString();
+
+};
+
+checkPassword('43657', 34567);
