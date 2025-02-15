f0206bb1f4a27375225e2a90807393fbb9927dd7
Irina Vinitskaia <kreneva.i@yandex.ru>
2025-02-15 18:28:30 +0300

    refactor: add comment

--- 8-Cryptio/index.js
+++ 8-Cryptio/index.js
@@ -1,6 +1,7 @@
+// не поняла, зачем строку переводить в массив, если в строке нам доступна длина и возможность итерации
 const crypto = (password) => {
   let res = "";
-  let passString = password.toString();
+  let passString = password.toString(); // на случай, если передали число
   if(passString.length % 2 === 0) {
     for (let i = 0; i < passString.length; i+=2) {
       res += passString[i+1] + passString[i];
