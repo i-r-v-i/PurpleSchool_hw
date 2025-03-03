f5d0cd63ffaee44e9063f1060697f91c89290422
Irina Vinitskaia <kreneva.i@yandex.ru>
2025-03-03 13:21:25 +0300

    refactor: formatted

--- 7-Converter/index.js
+++ 7-Converter/index.js
@@ -1,60 +1,39 @@
 const CURRENCY_LIST = {
-  USD: { sum: 90, marker: '$'},
-  EUR: { sum: 100, marker: '€' },
-  RUB: { sum: 1, marker: '₽' },
-}
+  USD: { sum: 90, marker: "$" },
+  EUR: { sum: 100, marker: "€" },
+  RUB: { sum: 1, marker: "₽" },
+};
 
-function isValidCurrency(currency){
+function isValidCurrency(currency) {
   const isExists = Object.keys(CURRENCY_LIST).includes(currency);
-  if(!isExists){
-      console.log(`Валюта [${currency}] не поддерживается для обмена`);
+  if (!isExists) {
+    console.log(`Валюта [${currency}] не поддерживается для обмена`);
   }
   return isExists;
 }
 
-// const taggetRate = (from, to) => {
-//   let tagget;
-//   switch (true) {
-//     case from === "RUB" && to === "USD":
-//       return tagget=USDToRub;
-//     case from === "USD" && to === "RUB":
-//       return tagget=rubToUSD;
-//     case from === "EURO" && to === "RUB":
-//       return tagget=rubToEuro;
-//     case from === "RUB" && to === "EURO":
-//       return tagget=euroToRub;
-//     case from === "USD" && to === "EURO":
-//       return tagget=euroToUsd;
-//     case from === "EURO" && to === "USD":
-//       return tagget=USDToEuro;
-//     default:
-//       return tagget=0;
-//   }
-// }
-
 const convertMoney = (sum, from, to) => {
   from = from.toUpperCase();
   to = to.toUpperCase();
- 
+
   if (from === to) {
     return `Входящая и исходящая валюта одинаковы. Конвертация невозможна`;
-}
+  }
 
-// Проверяем наличие входящей валюты для обмена
-if (!isValidCurrency(from)) {
+  // Проверяем наличие входящей валюты для обмена
+  if (!isValidCurrency(from)) {
     return null;
-}
-// Проверяем наличие исходящей валюты для обмена
-if (!isValidCurrency(to)) {
+  }
+  // Проверяем наличие исходящей валюты для обмена
+  if (!isValidCurrency(to)) {
     return null;
-}
+  }
   // return sum * taggetRate() || null;
   const { sum: a, marker: im } = CURRENCY_LIST[from]; // im = inMarker
   const { sum: b, marker: om } = CURRENCY_LIST[to]; // om = outMarker
 
-    return `В результате обмена ${sum}${im} Вы получите ${(sum * a / b).toFixed(2)}${om}`
+  return `В результате обмена ${sum}${im} Вы получите ${((sum * a) / b).toFixed(2)}${om}`;
 };
-
 
 console.log(convertMoney(1, "eur", "ghj"));
 console.log(convertMoney(1, "eur", "eur"));
