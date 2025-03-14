58140e906f6c2f33d65450b357908bda6ef6509a
Irina Vinitskaia <kreneva.i@yandex.ru>
2025-03-14 11:49:17 +0300

    refactor: update some methods after revew

--- 15-Task-object/index.js
+++ 15-Task-object/index.js
@@ -2,29 +2,37 @@ const ToDoList = {
   tasks: [
     { title: "Помыть посуду", id: 1, priority: 1 },
   ],
+  getTaskById () {
+    const task = this.tasks.find((task) => task.id === id);
+    if(!task) {
+        console.log(`задача с id = ${id} не найдена`);
+        return;
+    }
+    return task;
+  },
   addTask: function (title='сделать...', priority=2) {
     const newId = this.tasks.length + 1;
     this.tasks.push({ title, id: newId, priority });
     return this.tasks;
   },
-  removeTask: function (id) {
-    this.tasks = this.tasks.filter(task => task.id !== id);
-    return this.tasks;
-  },
-  updateTask: function ({title, id, priority}) {
-    const curTask = this.tasks.find(task => task.id === id);
-    if(curTask) {
-      if(title) {
-        curTask.title = title;
-      }
-      if(priority) {
-        curTask.priority = priority;
-      }
-    } else {
-      console.log("Задача с данным id не найдена");
+  removeTask () {
+    const task = this.getTaskById(id);
+    if(task){
+        this.tasks = this.tasks.filter(task => task.id !== id);
     }
-    return curTask;
-  },
+    return this.tasks;
+  }, 
+  updateTask (data) {
+    const { id, ...otherData} = data
+    const task = this.getTaskById(id);
+    if(Object.keys(otherData).length === 0){
+        return task;
+    }
+    Object.assign(task, otherData);
+    console.log(`задача с id = ${ id } - успешно обновлена`);
+    return task;
+},
+
   sortTasksByPriority: function () {
     return this.tasks.sort((a, b) => a.priority - b.priority)
   },
